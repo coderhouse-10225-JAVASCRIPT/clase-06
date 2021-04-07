6696d8e1af5ec8a84de215606f3dd5d5273ea302
alejandro medici <el.aleyo@gmail.com>
2021-04-06 21:39:55 -0300

    recorrer arrays

--- js/app.js
+++ js/app.js
@@ -7,8 +7,35 @@ for (let index = 0; index < cantidad; index++) {
     let nombre = prompt("Agrega un nombre");
 
     listadeNombres.push(nombre);
+
 }
 
 
 console.log("La longitud del array es: " + listadeNombres.length);
 console.log("El array es: " + listadeNombres.toString());
+
+let primerElemento = listadeNombres.slice(0,1);
+let ultimoElemento = listadeNombres.slice(listadeNombres.length-1);
+
+console.log("El primer elemento del array es: " + primerElemento);
+console.log("El ultimo elemento del array es: " + ultimoElemento);
+
+console.log("La longitud del array es: " + listadeNombres.length);
+
+
+let nuevaLista = listadeNombres.concat(primerElemento);
+
+console.log("El nuevo array es: " + nuevaLista.toString());
+
+let index = 0;
+let indexEnd = listadeNombres.length-1;
+do {
+    const element = listadeNombres[index];
+    const elementEnd = listadeNombres[indexEnd];
+
+    index+=1;
+    indexEnd-=1;
+
+} while (index < listadeNombres.length);
+    
+ 
